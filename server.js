581d4da6993c0bf14e6ac3c1b15a762424f5f8ae
NguyenQui <nguyendinhqui1029@gmail.com>
2021-03-05 12:05:31 +0700

    update server

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const app = express();
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 
-const ConnectMongoDB = require('./app');
-ConnectMongoDB.connect(function (err) {
-    db.collection("user").find({ userName: "qnguyen1997" }, { userName: 0, name: 1 })
-        .toArray().
-        then(items => {
-            console.log('All items', items);
-            connection.close();
-        });
-});
+// const ConnectMongoDB = require('./app');
+// ConnectMongoDB.connect(function (err) {
+//     db.collection("user").find({ userName: "qnguyen1997" }, { userName: 0, name: 1 })
+//         .toArray().
+//         then(items => {
+//             console.log('All items', items);
+//             connection.close();
+//         });
+
+const port = process.env.PORT || 3000;
+
+app.get('', function (req, res) {
+    {
+        res.send("aaaaa")
+    }
+})
+app.listen(port, () => {
+    console.log("start servers")
+})
