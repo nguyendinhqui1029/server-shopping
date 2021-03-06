468b79f2a2ed0e83a7f5a04414eb6172b2916d07
NguyenQui <nguyendinhqui1029@gmail.com>
2021-03-06 15:41:23 +0700

    update 2

---
+++ models/test.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var testSchema = new Schema({
+    name: String,
+});
+module.exports = mongoose.model('test', testSchema);
--- server.js
+++ server.js
@@ -14,9 +14,24 @@ app.use("/public", express.static(path.join(__dirname, 'public')));
 //         });
 
 const port = process.env.PORT || 3000;
-
+const testModel = require("./models/test")
+app.get("/name/:name", function (req, res) {
+    const mongoose = require("mongodb");
+    const MongoClient = mongoose.MongoClient;
+    const uri = "mongodb+srv://quinguyen:SVVQAO53kG7ZpjFO@cluster0.6uvbx.mongodb.net/personalweb?retryWrites=true&w=majority";
+    const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoClient.connect(err => {
+        mongoClient.db("personalweb").collection("test").insertOne({ name: req.params.name }).then(() => {
+            mongoClient.close();
+        });
+    });
+    res.send({ status: true })
+})
 app.get('', function (req, res) {
     res.sendFile('views/index.html', { root: __dirname })
+})
+app.get("/getnhac", (req, res) => {
+    res.sendFile('public/upload/nhac.mp3', { root: __dirname })
 })
 app.listen(port, () => {
     console.log("start servers")
