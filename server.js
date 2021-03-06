0263e23dfa56d68ff0987d71f5db04bd82ed0283
NguyenQui <nguyendinhqui1029@gmail.com>
2021-03-06 10:18:17 +0700

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
-
+app.use("/public", express.static(path.join(__dirname, 'public')));
 // const ConnectMongoDB = require('./app');
 // ConnectMongoDB.connect(function (err) {
 //     db.collection("user").find({ userName: "qnguyen1997" }, { userName: 0, name: 1 })
@@ -15,9 +16,7 @@ const bodyparser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 app.get('', function (req, res) {
-    {
-        res.send("aaaaa")
-    }
+    res.sendFile('views/index.html', { root: __dirname })
 })
 app.listen(port, () => {
     console.log("start servers")
